0f99bbfd0aadd786d0dca82a7e5cdfacad280738
Thauã <thauacerqueira07@gmail.com>
2026-03-21 22:10:22 -0300

    atualizacao

--- src/obj/Debug/net10.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net10.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed38a1934056fd528e368f2738fc01f1297154b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
