5c33c600d4faf1f8693232b4febfd94f93235fc4
feliped <00.felipe.daio.00@gmail.com>
2026-03-25 16:19:54 -0300

    program cs, certo

--- src/Program.cs
+++ src/Program.cs
@@ -26,6 +26,7 @@ builder.Services.AddScoped<EventoService>();
 
 var app = builder.Build();
 
+app.MapControllers();
 app.MapPost("/api/usuarios", async (Usuario usuario, UsuarioRepository repository) =>
 
 {
--- src/obj/Debug/net10.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net10.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c9541b927ddd02200ebbc227c6e1e20fcf0508")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c6e2c6c0ee804578f9273d0cbeb1b67b71c32f3")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
