104cf467bc1c0ccd1a8e97064f8ea0867b54fe0d
feliped <00.felipe.daio.00@gmail.com>
2026-03-25 16:04:22 -0300

    att

--- src/obj/Debug/net10.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net10.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2340236fc1fe4b33fc625dca6f39f4983b28d3a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9c9541b927ddd02200ebbc227c6e1e20fcf0508")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
