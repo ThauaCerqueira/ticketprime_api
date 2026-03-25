9487e046c17fa3475a732170b1d8a92291d13cf3
feliped <00.felipe.daio.00@gmail.com>
2026-03-25 13:30:58 -0300

    Arquivos do git ignore

--- src/obj/Debug/net10.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net10.0/src.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4bc4d9be98510ab3d53d0d57afddcd40e54d07d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee136315c967452a285fd9eec7349ad44d9b7efa")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
