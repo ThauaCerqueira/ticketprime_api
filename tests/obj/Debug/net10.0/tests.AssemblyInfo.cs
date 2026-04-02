b13ec1c3e5f7c54d7532ea6b88272be43ea53baf
Thauã <thauacerqueira07@gmail.com>
2026-04-02 17:40:37 -0300

    att

--- tests/obj/Debug/net10.0/tests.AssemblyInfo.cs
+++ tests/obj/Debug/net10.0/tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96df61d5f0abd4c18395fdeec2b44b69a6590cc0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c6573e916f324663e742ee39fd3993bb12fbb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ui/TicketPrime.Web/obj/Debug/net10.0/TicketPrime.Web.AssemblyInfo.cs
+++ ui/TicketPrime.Web/obj/Debug/net10.0/TicketPrime.Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TicketPrime.Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+95e38481848e731d2893ef3aaa20f0d98dcca185")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+47c6573e916f324663e742ee39fd3993bb12fbb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("TicketPrime.Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TicketPrime.Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
