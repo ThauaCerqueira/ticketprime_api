10c8217440196051e5bd9f49b6ab91c1976fe398
Giulia Massafra <giulia@ticketprime.com>
2026-05-08 01:02:57 -0300

    refactor: substitui chamadas HTTP por injeção direta de serviços no frontend

--- ui/TicketPrime.Web/Program.cs
+++ ui/TicketPrime.Web/Program.cs
@@ -1,50 +1,53 @@
 using TicketPrime.Web.Components;
 using TicketPrime.Web.Services;
 using src.Service;
-using src.Repositories;
 using src.Infrastructure;
+using src.Infrastructure.Repository;
 using src.Infrastructure.IRepository;
- 
+
 var builder = WebApplication.CreateBuilder(args);
- 
+
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
     ?? "Server=localhost;Database=TicketPrime;Integrated Security=True;TrustServerCertificate=True;";
- 
+
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
- 
+
+// Infrastructure
 builder.Services.AddSingleton(new DbConnectionFactory(connectionString));
+builder.Services.AddScoped<IUsuarioRepository, UsuarioRepository>();
+builder.Services.AddScoped<IEventoRepository, EventoRepository>();
+builder.Services.AddScoped<IReservaRepository, ReservaRepository>();
 builder.Services.AddScoped<ICupomRepository, CupomRepository>();
+
+// Services
+builder.Services.AddScoped<UsuarioService>();
+builder.Services.AddScoped<AuthService>();
+builder.Services.AddScoped<EventoService>();
+builder.Services.AddScoped<ReservaService>();
 builder.Services.AddScoped<CupomService>();
- 
+
+// Session
 builder.Services.AddSingleton<SessionService>();
- 
-builder.Services.AddTransient(sp =>
-{
-    var session = sp.GetRequiredService<SessionService>();
-    var client = new HttpClient { BaseAddress = new Uri("http://localhost:5164") };
 
-    if (!string.IsNullOrEmpty(session.Token))
-        client.DefaultRequestHeaders.Authorization =
-            new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", session.Token);
+// HttpClient for API calls
+var apiBaseUrl = builder.Configuration["ApiSettings:BaseUrl"] ?? "http://localhost:5164";
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(apiBaseUrl) });
 
-    return client;
-});
- 
 var app = builder.Build();
- 
+
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     app.UseHsts();
 }
- 
+
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
 app.UseAntiforgery();
 app.MapStaticAssets();
- 
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
- 
+
 app.Run();
